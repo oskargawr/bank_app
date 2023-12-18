e1651b3075b6f87258cd1389a455fa5c80903313
Oskar Gawryszewski <oskargawryszewski@MacBook-Air-Oskar-2.local>
2023-12-18 13:00:39 +0100

    test wysylania historii na maila

--- app/Konto.py
+++ app/Konto.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 class Konto:
     def __init__(self):
         self.saldo = 0
@@ -17,3 +20,12 @@ class Konto:
             self.saldo -= kwota + self.oplata_za_przelew_ekspresowy
             self.history += [-kwota]
             self.history += [-self.oplata_za_przelew_ekspresowy]
+
+    def wyslij_historie_na_maila(self, SMTPConnection, adresat):
+        temat = f"Wyciag z dnia {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
+        if hasattr(self, "nip"):
+            tresc = "Historia konta twojej firmy: " + str(self.history)
+            return SMTPConnection.wyslij(temat, tresc, adresat)
+        else:
+            tresc = "Historia konta: " + str(self.history)
+            return SMTPConnection.wyslij(temat, tresc, adresat)
