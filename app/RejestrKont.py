40e33c16d8cfc30deaa58bd124926583c3ad1ec2
Oskar Gawryszewski <oskargawryszewski@MacBook-Air-Oskar-2.local>
2024-01-08 15:20:22 +0100

    narzedzie cucumber

--- app/RejestrKont.py
+++ app/RejestrKont.py
@@ -1,7 +1,12 @@
 from .Konto import Konto
+from .KontoOsobiste import KontoOsobiste
+from pymongo import MongoClient
 
 
 class RejestrKont:
+    client = MongoClient("localhost", 27017)
+    db = client["bank_app"]
+    collection = db["konta"]
     lista = []
 
     @classmethod
@@ -26,3 +31,26 @@ class RejestrKont:
                 self.lista.remove(konto)
                 return True
         return False
+
+    @classmethod
+    def save(cls):
+        cls.collection.delete_many({})
+        for konto in cls.lista:
+            cls.collection.insert_one(
+                {
+                    "imie": konto.imie,
+                    "nazwisko": konto.nazwisko,
+                    "pesel": konto.pesel,
+                    "saldo": konto.saldo,
+                    "history": konto.history,
+                }
+            )
+
+    @classmethod
+    def load(cls):
+        cls.lista = []
+        for konto in cls.collection.find():
+            acc = KontoOsobiste(konto["imie"], konto["nazwisko"], konto["pesel"])
+            acc.saldo = konto["saldo"]
+            acc.history = konto["history"]
+            cls.lista.append(acc)
